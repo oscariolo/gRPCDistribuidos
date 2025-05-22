c37e3e1df0a6e38fc29ccc7e8dd12a5d3f681fd8
Oscariolo <oscariolopinop21@gmail.com>
2025-05-21 22:49:16 -0500

    OO-MOD-Server and client communicating by gRPC

--- gRPC_Client/app/src/main/java/grpc_client/App.java
+++ gRPC_Client/app/src/main/java/grpc_client/App.java
@@ -3,12 +3,81 @@
  */
 package grpc_client;
 
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.example.proto.services.*;
+
+import io.grpc.Channel;
+import io.grpc.StatusRuntimeException;
+import io.grpc.Grpc;
+import io.grpc.InsecureChannelCredentials;
+import io.grpc.ManagedChannel;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+  private static final Logger logger = Logger.getLogger(App.class.getName());
+
+  private final ProductServiceGrpc.ProductServiceBlockingStub blockingStub;
+
+  public App(Channel channel) {
+    // 'channel' here is a Channel, not a ManagedChannel, so it is not this code's responsibility to
+    // shut it down.
+
+    // Passing Channels to code makes code easier to test and makes it easier to reuse Channels.
+    blockingStub = ProductServiceGrpc.newBlockingStub(channel);
+  }
+
+  /** Say hello to server. */
+  public void pedirProducto(String productName) {
+    logger.info("Pidiendo producto...");
+    ProductRequest request = ProductRequest.newBuilder().setName(productName).build();
+    ProductResponse response;
+    try {
+      response = blockingStub.getProduct(request);
+    } catch (StatusRuntimeException e) {
+      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+      return;
+    }
+    logger.info(response.getDescription());
+  }
+
+ 
+  public static void main(String[] args) throws Exception {
+    String user = "Usuario Grupo 6";
+    // Access a service running on the local machine on port 50051
+    String target = "localhost:50051";
+    // Allow passing in the user and target strings as command line arguments
+    if (args.length > 0) {
+      if ("--help".equals(args[0])) {
+        System.err.println("Usage: [name [target]]");
+        System.err.println("");
+        System.err.println("  name    The name you wish to be greeted by. Defaults to " + user);
+        System.err.println("  target  The server to connect to. Defaults to " + target);
+        System.exit(1);
+      }
+      user = args[0];
+    }
+    if (args.length > 1) {
+      target = args[1];
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    // Create a communication channel to the server, known as a Channel. Channels are thread-safe
+    // and reusable. It is common to create channels at the beginning of your application and reuse
+    // them until the application shuts down.
+    //
+    // For the example we use plaintext insecure credentials to avoid needing TLS certificates. To
+    // use TLS, use TlsChannelCredentials instead.
+    ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create())
+        .build();
+    try {
+      App client = new App(channel);
+      client.pedirProducto("Cebolla");
+    } finally {
+      // ManagedChannels use resources like threads and TCP connections. To prevent leaking these
+      // resources the channel should be shut down when it will no longer be used. If it may be used
+      // again leave it running.
+      channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
     }
+  }
 }
--- gRPC_Server/app/src/main/java/grpc_trabajo/impl/ProductServiceImpl.java
+++ gRPC_Server/app/src/main/java/grpc_trabajo/impl/ProductServiceImpl.java
@@ -6,15 +6,26 @@ import io.grpc.stub.StreamObserver;
 public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBase {
     @Override
     public void getProduct(ProductRequest request, StreamObserver<ProductResponse> responseObserver) {
+
+       
+
         // Simulate a product lookup
         String productName = request.getName();
-        String productdescription = "Product " + productName;
+        String productdescription = "";
         double productPrice = 19.99;
+
+         if(productName.equals("Cebolla")){
+            productdescription = "Las cebollas son como los ogros, tienen capas";
+            productPrice = 1.99;
+         }else{
+            productdescription = "No se ha encontrado el producto pedido" + productName;
+            productPrice = 0.00;
+         }
 
         // Create the response
         ProductResponse response = ProductResponse.newBuilder()
                 .setName(productName)
-                .setDescription("Descripcion de ejemplo " + productdescription)
+                .setDescription(productdescription)
                 .setPrice(productPrice)
                 .build();
 
